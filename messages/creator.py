9ddf68edb4b91f8e058cf20a3d1ab36d8c92e286
NJaki <vividindia.neog@gmail.com>
2022-07-27 11:14:47 +0530

    Update creator.py

--- messages/creator.py
+++ messages/creator.py
@@ -2,13 +2,13 @@ from telegram.utils.helpers import escape_markdown as es
 
 
 def start_msg(name):
-    msg = f"""*Hey {es(name,version=2)}* ✋✋ *welcome to Jiosaavn downloader bot* ⚡⚡\n
-    _Just send me a jiosaavn song or album link I will send you the audio_\n
-made by @phantom2152 😈😈"""
+    msg = f"""*Hey {es(name,version=2)}* ✋✋ *welcome to Pagalworldsongs Bot* ⚡⚡\n
+    _Just send me a Jiosaavn song or album link and I will send you the audio_\n
+made by @pagalworldsongs 😈😈"""
     return msg
 
 
 def help_msg():
     help = """ℹ️⁉⁉ *help*\n
-*just send me a jiosaavn song,album or playlist link, I will send you the audio with lyrics*⚡⚡"""
+*just send me a jiosaavn song, album or playlist link, I will send you the audio with lyrics*⚡⚡"""
     return help
